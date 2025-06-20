df64d24d987a1a9337b4158f8401999532ba1d93
Agustín Martin <agusmartin113@gmail.com>
2025-06-20 21:13:22 +0000

    src/app/quiz/[quizId]/page.tsx Type error: Type '{ params: { quizId: str

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type {NextConfig} from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
+  basePath: isProd ? '/<nombre_de_tu_repositorio>' : '',
   typescript: {
     ignoreBuildErrors: true,
   },
